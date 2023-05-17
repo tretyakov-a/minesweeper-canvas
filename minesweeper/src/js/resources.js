7bd6cc4068e40632c117a150ce2321d7bf943178
Alexander Tretyakov <tpetyakov@gmail.com>
2023-05-17 11:03:29 +0300

    refactor: remove unused resources

--- minesweeper/src/js/resources.js
+++ minesweeper/src/js/resources.js
@@ -1,21 +1,23 @@
 import mineSrc from '@src/assets/mine.svg';
-import closedSrc from '@src/assets/closed.svg';
-import openedSrc from '@src/assets/opened.svg';
-import flaggedSrc from '@src/assets/flagged.svg';
 import flagSrc from '@src/assets/flag.svg';
 import playingSrc from '@src/assets/playing.png';
 import loseSrc from '@src/assets/lose.png';
 import winSrc from '@src/assets/win.png';
 
+export const RESOURCES = {
+  MINE: 'mine',
+  FLAG: 'flag',
+  PLAYING: 'playing',
+  LOSE: 'lose',
+  WIN: 'win',
+};
+
 export const resources = {
-  mine: mineSrc,
-  closed: closedSrc,
-  opened: openedSrc,
-  flagged: flaggedSrc,
-  flag: flagSrc,
-  playing: playingSrc,
-  lose: loseSrc,
-  win: winSrc,
+  [RESOURCES.MINE]: mineSrc,
+  [RESOURCES.FLAG]: flagSrc,
+  [RESOURCES.PLAYING]: playingSrc,
+  [RESOURCES.LOSE]: loseSrc,
+  [RESOURCES.WIN]: winSrc,
 };
 
 const loadImage = (imageSrc) =>
